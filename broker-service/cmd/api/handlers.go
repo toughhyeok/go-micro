8153387fc75d02825eb814505b1e7316e3bbf725
junhyeok <toughhyeok96@gmail.com>
2022-11-30 00:18:29 +0900

    first commit

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+}
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "Hit the broker",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
